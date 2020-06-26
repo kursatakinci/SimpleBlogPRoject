7e7f7d50f3ca9e11ff9daf970b510585b92798fb
Kürşat Akıncı <kursatakinci.126@gmail.com>
2020-06-26 07:17:39 +0300

    autofac added.

--- SimpleBlogProject/Startup.cs
+++ SimpleBlogProject/Startup.cs
@@ -1,15 +1,14 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using SimpleBlogProject.Core.Elements;
+using SimpleBlogProject.Core.Helper.TypeHelper;
 
 namespace SimpleBlogProject
 {
@@ -21,16 +20,30 @@ namespace SimpleBlogProject
         }
 
         public IConfiguration Configuration { get; }
+        public ILifetimeScope AutofacContainer { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers();         
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            var registrarTypes = TypeFinder.FindClassesOfType<IDependencyRegistrar>();
+            var drInstances = new List<IDependencyRegistrar>();
+            foreach (var registrarType in registrarTypes)
+                drInstances.Add((IDependencyRegistrar)Activator.CreateInstance(registrarType));
+
+            foreach (var drInstance in drInstances)
+                drInstance.Register(builder);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            this.AutofacContainer = app.ApplicationServices.GetAutofacRoot();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
