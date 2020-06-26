5f06610a97459fc0e4b12dbd983fd6325e1eec3f
Kürşat Akıncı <kursatakinci.126@gmail.com>
2020-06-26 07:22:30 +0300

    hashing algorithm added

---
+++ SimpleBlogProject.Core/Helper/Cryption/HashingHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SimpleBlogProject.Core.Helper.Cryption
+{
+    public static class HashingHelper
+    {
+        public static string Hash(string originalString)
+        {
+            using(SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(originalString));
+                return Convert.ToBase64String(bytes);
+            }
+        }
+    }
+}
