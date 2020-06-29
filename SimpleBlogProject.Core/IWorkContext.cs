7f35ffdbe21a0f4e4a45dcd2cf1ccf5024a42f88
Kürşat Akıncı <kursatakinci.126@gmail.com>
2020-06-29 09:00:37 +0300

    ad work context added to project

---
+++ SimpleBlogProject.Core/IWorkContext.cs
@@ -0,0 +1,13 @@
+﻿using SimpleBlogProject.Contract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleBlogProject.Core
+{
+    public interface IWorkContext
+    {
+        UserInfoDto WorkingUserInfo { get; }
+        BloggerDto WorkingBlogger { get; }
+    }
+}
