8961e104001726f45cd21856ab4409829a99d68b
Kürşat Akıncı <kursatakinci.126@gmail.com>
2020-06-29 09:00:58 +0300

    Workcontext concrete class added to project

---
+++ SimpleBlogProject.Framework/WorkContext.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using SimpleBlogProject.Contract;
+using SimpleBlogProject.Core;
+using System;
+
+namespace SimpleBlogProject.Framework
+{
+    public class WorkContext : IWorkContext
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public WorkContext(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public UserInfoDto WorkingUserInfo 
+        { 
+            get
+            {
+                var userInfo = _httpContextAccessor.HttpContext.Request.Headers["UserInfo"].ToString();
+
+                if (string.IsNullOrEmpty(userInfo))
+                    return null;
+
+                UserInfoDto userInfoDto = null;
+
+                try
+                {
+                    userInfoDto = JsonConvert.DeserializeObject<UserInfoDto>(userInfo);
+                }
+                catch (Exception ex)
+                {
+                    userInfoDto = null;
+                }
+
+                return userInfoDto;
+            }
+        }
+
+        public BloggerDto WorkingBlogger
+        {
+            get
+            {
+                var bloggerInfo = _httpContextAccessor.HttpContext.Request.Headers["BloggerInfo"].ToString();
+
+                if (string.IsNullOrEmpty(bloggerInfo))
+                    return null;
+
+                BloggerDto bloggerInfoDto = null;
+
+                try
+                {
+                    bloggerInfoDto = JsonConvert.DeserializeObject<BloggerDto>(bloggerInfo);
+                }
+                catch (Exception ex)
+                {
+                    bloggerInfoDto = null;
+                }
+
+                return bloggerInfoDto;
+            }
+        }
+    }
+}
