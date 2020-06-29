bd03ae86d6c11c1ff7ac3b7e03b6d7a3481421c3
Kürşat Akıncı <kursatakinci.126@gmail.com>
2020-06-29 08:58:42 +0300

    dependency registrar added to framework layer

--- SimpleBlogProject.Framework/Dependencies/DependencyRegistrar.cs
+++ SimpleBlogProject.Framework/Dependencies/DependencyRegistrar.cs
@@ -1,7 +1,12 @@
 ﻿using Autofac;
+using SimpleBlogProject.Core;
+using SimpleBlogProject.Core.Caching;
 using SimpleBlogProject.Core.Elements;
+using SimpleBlogProject.Framework.Filters;
 using SimpleBlogProject.Repository;
 using SimpleBlogProject.Service.Blog;
+using SimpleBlogProject.Service.Blogger;
+using SimpleBlogProject.Service.User;
 
 namespace SimpleBlogProject.Framework.Dependencies
 {
@@ -14,8 +19,13 @@ namespace SimpleBlogProject.Framework.Dependencies
 
         public void Register(ContainerBuilder builder)
         {
-            builder.RegisterType<BlogService>().As<IBlogService>().SingleInstance();
+            builder.RegisterType<MemoryCacheManager>().As<ICacheManager>().SingleInstance();
             builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().SingleInstance();
+            builder.RegisterType<BlogService>().As<IBlogService>().SingleInstance();
+            builder.RegisterType<BlogPostService>().As<IBlogPostService>().SingleInstance();
+            builder.RegisterType<UserInfoService>().As<IUserInfoService>().SingleInstance();
+            builder.RegisterType<BloggerService>().As<IBloggerService>().SingleInstance();
+            builder.RegisterType<WorkContext>().As<IWorkContext>().InstancePerLifetimeScope();
         }
     }
 }
