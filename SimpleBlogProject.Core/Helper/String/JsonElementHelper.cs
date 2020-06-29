7a65f50e3243b16e9f735421624cbeee619d196f
Kürşat Akıncı <kursatakinci.126@gmail.com>
2020-06-29 09:00:20 +0300

    Json element helper added to project

---
+++ SimpleBlogProject.Core/Helper/String/JsonElementHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace SimpleBlogProject.Core.Helper.String
+{
+    public static class JsonElementHelper
+    {
+        public static string GetString(JsonElement jsonElement)
+        {
+            string json = JsonSerializer.Serialize(jsonElement);
+
+            return json;
+        }
+    }
+}
